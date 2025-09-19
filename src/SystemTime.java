155b9c778248a5dfe62864588c8c19779f2f09b7
Duc Manh <ducmanh@Duc-Manh.local>
2025-09-19 18:55:18 +0700

    second-commit

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class SystemTim {
+public class SystemTime {
     public static void main(String[] args) throws Exception {
         Date now = new Date();
         System.out.println("Now is: " + now);
