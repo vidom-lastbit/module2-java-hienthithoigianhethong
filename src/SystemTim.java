ff79fb7b39166b1eb261e4ccc9520c6e516c088c
Duc Manh <ducmanh@Duc-Manh.local>
2025-09-19 18:52:37 +0700

    first commit

---
+++ src/SystemTim.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTim {
+    public static void main(String[] args) throws Exception {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
